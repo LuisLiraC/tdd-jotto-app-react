b0296a5a5d631c2a25be40eb30e8c3015d4aab3e
LuisLiraC <luis_ssbm@hotmail.com>
2020-12-31 12:48:12 -0600

    add getLetterMatchCount

---
+++ src/__test__/helpers/index.test.js
@@ -0,0 +1,20 @@
+import { getLetterMatchCount } from '../../helpers/index'
+
+describe("getLetterMatchCount", () => {
+  const secretWord = "party"
+
+  test("returns correct when there are no matching letters", () => {
+    const letterMatchCount = getLetterMatchCount('bones', secretWord)
+    expect(letterMatchCount).toBe(0)
+  })
+
+  test("returns the correct count where there 3 matching letter", () => {
+    const letterMatchCount = getLetterMatchCount('train', secretWord)
+    expect(letterMatchCount).toBe(3)
+  })
+
+  test("returns correct count when there are duplicate letter in the guess", () => {
+    const letterMatchCount = getLetterMatchCount("parka", secretWord)
+    expect(letterMatchCount).toBe(3)
+  })
+})
